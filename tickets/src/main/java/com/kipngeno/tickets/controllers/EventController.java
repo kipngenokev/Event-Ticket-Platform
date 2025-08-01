0e21c29885b1b79d04ce2e00e630fb0dd3ec62d7
brian070069 <kkipngeno409@gmail.com>
2025-08-01 16:07:58 +0300

    Created eventController class, implemented create eventendpoint

---
+++ tickets/src/main/java/com/kipngeno/tickets/controllers/EventController.java
@@ -0,0 +1,43 @@
+package com.kipngeno.tickets.controllers;
+
+import com.kipngeno.tickets.domain.CreateEventRequest;
+import com.kipngeno.tickets.domain.dtos.CreateEventRequestDto;
+import com.kipngeno.tickets.domain.dtos.CreateEventResponseDto;
+import com.kipngeno.tickets.domain.entities.Event;
+import com.kipngeno.tickets.mappers.EventMapper;
+import com.kipngeno.tickets.services.EventService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/api/v1/events")
+@RequiredArgsConstructor
+
+public class EventController {
+
+    private final EventMapper eventMapper;
+
+    private final EventService eventService;
+
+    @PostMapping
+    public ResponseEntity<CreateEventResponseDto> createEvent(
+            @AuthenticationPrincipal Jwt jwt,
+            @Valid @RequestBody CreateEventRequestDto createEventRequestDto
+    ) {
+        CreateEventRequest createEventRequest = eventMapper.fromDto(createEventRequestDto);
+        UUID userId = UUID.fromString(jwt.getSubject());
+        Event createdEvent = eventService.createEvent(userId, createEventRequest);
+        CreateEventResponseDto createEventResponseDto = eventMapper.toDto(createdEvent);
+        return new ResponseEntity<>(createEventResponseDto, HttpStatus.CREATED);
+    }
+}
