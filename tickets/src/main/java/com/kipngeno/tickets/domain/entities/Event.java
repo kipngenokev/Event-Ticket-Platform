017fcdc391c8c5be1af729870e77b5c2d01202de
brian070069 <kkipngeno409@gmail.com>
2025-08-06 23:55:22 +0300

    used create event button,stuck on CORS error

--- tickets/src/main/java/com/kipngeno/tickets/domain/entities/Event.java
+++ tickets/src/main/java/com/kipngeno/tickets/domain/entities/Event.java
@@ -28,11 +28,11 @@ public class Event {
     @Column(name = "name " ,updatable = false, nullable = false)
     private String name;
 
-    @Column(name =" start ")
+    @Column(name =" event_start ")
     private LocalDateTime start;
 
 
-    @Column(name = "end ")
+    @Column(name = "event_end ")
     private LocalDateTime end;
 
     @Column(name = "venue",nullable = false)
